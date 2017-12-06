f18896e77747fb28f5a4a155abc5615e61ad9fc4
Razerware <lizhengyin1115@163.com>
2017-12-07 01:36:23 +0800

    1.1

--- main.go
+++ main.go
@@ -5,18 +5,21 @@ import (
 	"os"
 	"os/signal"
 	"rpc_client/client"
+	"time"
 )
 
 func main() {
 	c := make(chan os.Signal)
 	signal.Notify(c)
 	info:=client.HostInfo{1,"10.109.252.172"}
-	client.CollectData(info)
 	for {
 		select {
 		case <-c:
 			fmt.Println("get signal:", c)
 			os.Exit(1)
+		default:
+			client.CollectData(info)
+			time.Sleep(10*time.Second)
 		}
 	}
 }

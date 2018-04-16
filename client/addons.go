5bf5ec1e3cb6d1645e5a0e41a0f26e5419aeea2c
razerware <lizhengyin1115@163.com>
2018-04-16 14:59:03 +0800

    add addons

--- client/addons.go
+++ client/addons.go
@@ -15,14 +15,14 @@ func GetInternal() (int, string, string) {
 	for _, a := range addrs {
 		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				//ipnet.IP.String()
-				sql := fmt.Sprintf("SELECT * FROM `vm_info` where `ip`='%s'", ipnet.IP.String())
+				//ipnet.IP.String() 10.109.252.172
+				sql := fmt.Sprintf("SELECT * FROM `vm_info` where `ip`='%s'","10.109.252.172")
 				record := MysqlQuery(sql)
 				if len(record) > 0 {
 					v1, _ := record[0]["inner_id"].(int)
 					v2, _ := record[0]["ip"].(string)
 					v3, _ := record[0]["swarm_id"].(string)
-					glog.Info("Hostid is :",v1,"IP is: " ,v2, "Swarm_id is:",v3)
+					glog.Info("Hostid is :", v1, " IP is: ", v2, " Swarm_id is:", v3)
 					return v1, v2, v3
 				}
 			}
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 	"github.com/razerware/monitor_client/client"
 	"github.com/golang/glog"
+	"syscall"
 )
 
 func main() {
@@ -22,22 +23,36 @@ func main() {
 	info := client.HostInfo{hostid, hostip, swarmid}
 	glog.Info("Data collecting start...", info)
 	t := make(chan int)
-	go func(t chan int) {
-		for {
-			t <- 1
-			duration := 30 * time.Second
-			time.Sleep(duration)
-		}
-
-	}(t)
+	go timeCount(t)
+	go handleSignal()
 	for {
 		select {
-		case <-c:
-			glog.Exit("program exit")
-			os.Exit(1)
+		//case <-c:
+		//	glog.Exit("program exit")
+		//	os.Exit(1)
 		case <-t:
 			glog.Info("Collecting data...")
 			client.CollectData(info)
 		}
 	}
+}
+
+func timeCount(t chan int){
+	for {
+		t <- 1
+		duration := 30 * time.Second
+		time.Sleep(duration)
+	}
+}
+
+func handleSignal(){
+	signalChan := make(chan os.Signal, 1)
+	//os.Interrupt, os.Kill,
+	signal.Notify(signalChan, syscall.SIGTERM)
+	s:=<-signalChan
+	glog.Infof("Received SIGTERM, shutting down ",s)
+
+	exitCode := 0
+	glog.Infof("Exiting with %v", exitCode)
+	os.Exit(exitCode)
 }
